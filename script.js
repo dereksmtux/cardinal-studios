981e3e945665a25dd1914ccf8842bd3e13d93c05
dereksmtux <dereksmith2112@verizon.net>
2017-05-29 13:53:20 -0500

    store using jquery

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ $(document).ready(function() {
   $('.header').slideToggle(800);
   $('.content').fadeIn(2000)
   $('div.storeContainer').fadeIn(2000);
+//handler for expanding item
+  $('img.storeItem').click(function(){
+    $('div.storeItem').hide();
+    $(this).parent().show();
+    $('div.storeItem').toggleClass('expandedItem');
+    $('button.storeBackButton').show();
+  });
+//handler for store back button
+  $('button.storeBackButton').click(function(){
+    $('button.storeBackButton').hide();
+    $('div.storeItem').toggleClass('expandedItem');
+    $('div.storeItem').show();
+  });
 });
